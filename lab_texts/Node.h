ca1cd56b10d003f8c03b2449e8cdca1213c71af5
Хисматулина Карина <hkarinaf@gmail.com>
2020-03-13 14:35:11 +0300

    mkn lab

--- lab_texts/Node.h
+++ lab_texts/Node.h
@@ -1,16 +1,26 @@
 #pragma once
+#include <cassert>
 #include <string>
 class Node
 {
 public:
 	std::string data;
-	char* Next;	//указатель на след элем
-	char* down; //указатель на элемент в вышенаходящемся разделе
+	Node* Next;	//указатель на след элем
+	Node* down; //указатель на элемент в вышенаходящемся разделе
 	int level;
 
-	Node()
+	Node(std::string d) : data(d)
 	{
-
+		assert(d != "\0");
+		Next = NULL;
+		down = NULL;
+	}
+	Node(Node &tmp)
+	{
+		data = tmp.data;
+		Next = tmp.Next;
+		level = tmp.level;
+		data = tmp.data;
 	}
 };
 
@@ -20,21 +30,74 @@ private:
 	Node* First;
 	Node* End;
 
-public:
-	void Push_back(std::string data, int lvl)
+	Node* searchLastRight(Node* current)
 	{
+		if (current->Next == nullptr)
+			return current;
 
+		Node* tmp = new Node(*current);
+		while (tmp->Next != nullptr)
+		{
+			tmp = tmp->Next;
+		}
+		return tmp;
+	}
+
+	Node* searchLastLast(Node* current)
+	{
+		Node* tmp = searchLastRight(current);
+		if (tmp->down == nullptr) return tmp;
+		return searchLastLast(tmp->down);
+	}
+
+
+
+public:
+	void Push_back_current_level(std::string data)
+	{
+		if (First = NULL)
+		{
+			auto sym = new Node(data);
+			First = sym;
+			End = sym;
+			First->Next = NULL;
+		}
+		else
+		{
+			auto sym = new Node(data);
+			sym->level = End->level;
+			End->Next = sym;
+			End = End->Next;
+		}
+	}
+
+	void Push_back_next_level(std::string data)
+	{
+		if (First == NULL)
+		{
+			throw "you are dumn jopa!!!!!!!";
+		}
+		else
+		{
+			auto sym = new Node(data);
+			sym->level = (End->level + 1);
+			End->Next = sym;
+			End = End->Next;
+		}
 	}
 
 	void Pop_back()
 	{
-
+		assert(First != NULL);
+		
 	}
 
-	void Displsy()
+	void Display()
 	{
 
 	}
+
+	
 
 	~MyList()
 	{
---
+++ lab_texts/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Node.h"
+
+int main()
+{
+	return 0;
+}
